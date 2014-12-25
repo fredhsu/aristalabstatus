2e8cbba4a242f996a0bc4a72af58bde0ce60fe6a
Fred Hsu <fredlhsu@gmail.com>
2014-12-24 16:40:39 -0800

    Refactoring openstack

--- openstack.go
+++ openstack.go
@@ -1,4 +1,4 @@
-package main
+package aristalabstatus
 
 import (
       "github.com/rackspace/gophercloud"
@@ -9,16 +9,20 @@ import (
       "fmt"
 )
 
-func main() {
-    opts,err := openstack.AuthOptionsFromEnv()
+func getProvider() *gophercloud.ProviderClient{
+    authOpts,err := openstack.AuthOptionsFromEnv()
     if err != nil {
         fmt.Println(err)
     }
-    fmt.Println(opts)
-    provider, err := openstack.AuthenticatedClient(opts)
+    provider, err := openstack.AuthenticatedClient(authOpts)
     if err != nil {
         fmt.Println(err)
     }
+    return provider
+}
+
+func getNetworkClient() *gophercloud.ServiceClient {
+    provider := getProvider()
     client, err := openstack.NewNetworkV2(provider, gophercloud.EndpointOpts {
         Name: "neutron",
         Region: "RegionOne",
@@ -26,18 +30,30 @@ func main() {
     if err != nil {
         fmt.Println(err)
     }
-    shared := false
+    return client
+}
+
+func CreateNetworks() {
+    client := getNetworkClient()
     netopts := networks.CreateOpts{Name: "test_network_199", AdminStateUp: networks.Up}
     network, err := networks.Create(client, netopts).Extract()
     fmt.Println(network)
 
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func FindNetwork(uuid string) {
+    client := getNetworkClient()
+    shared := false
     listopts := networks.ListOpts{Shared: &shared}
     pager := networks.List(client, listopts)
-    err = pager.EachPage(func(page pagination.Page) (bool, error) {
+    err := pager.EachPage(func(page pagination.Page) (bool, error) {
         networkList, _ := networks.ExtractNetworks(page)
         for _, n := range networkList {
             fmt.Println(n)
-            if n.ID == network.ID {
+            if n.ID == uuid {
                 fmt.Println("!!! Network verified !!!")
                 //result := networks.Delete(client, n.ID)
                 //fmt.Println(result)
@@ -45,6 +61,30 @@ func main() {
         }
         return true, nil
     })
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func RemoveNetworks() {
+}
+
+func CheckNeutronEOS(uuids []string) {
+}
+func CheckNovaEOS(uuids []string) {
+}
+func CheckRtrEOS() {
 }
 
 
+func CreateRouter() {
+}
+
+func RemoveRouter() {
+}
+
+func CreateNova() {
+}
+
+func RemoveNova() {
+}
