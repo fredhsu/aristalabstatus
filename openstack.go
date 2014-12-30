57fc15adcb910d6e52e4c02c45b6bdb5c2100a8b
Fred Hsu <fredlhsu@gmail.com>
2014-12-30 15:04:29 -0800

    Got Nova and Neutron working, added unit tests

--- openstack.go
+++ openstack.go
@@ -1,102 +1,177 @@
 package aristalabstatus
 
 import (
-      "github.com/rackspace/gophercloud"
-      "github.com/rackspace/gophercloud/openstack"
-      //"github.com/rackspace/gophercloud/openstack/utils"
-      "github.com/rackspace/gophercloud/openstack/networking/v2/networks"
-      "github.com/rackspace/gophercloud/pagination"
-      "fmt"
+	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/openstack"
+	//"github.com/rackspace/gophercloud/openstack/utils"
+	"fmt"
+	"github.com/fredhsu/eapigo"
+	"github.com/rackspace/gophercloud/openstack/compute/v2/servers"
+	"github.com/rackspace/gophercloud/openstack/networking/v2/networks"
+	"github.com/rackspace/gophercloud/openstack/networking/v2/subnets"
+
+	"github.com/rackspace/gophercloud/pagination"
 )
 
-func getProvider() *gophercloud.ProviderClient{
-    authOpts,err := openstack.AuthOptionsFromEnv()
-    if err != nil {
-        fmt.Println(err)
-    }
-    provider, err := openstack.AuthenticatedClient(authOpts)
-    if err != nil {
-        fmt.Println(err)
-    }
-    return provider
+func getProvider() *gophercloud.ProviderClient {
+	authOpts, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		fmt.Println(err)
+	}
+	provider, err := openstack.AuthenticatedClient(authOpts)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return provider
 }
 
 func GetNetworkClient() *gophercloud.ServiceClient {
-    provider := getProvider()
-    client, err := openstack.NewNetworkV2(provider, gophercloud.EndpointOpts {
-        Name: "neutron",
-        Region: "RegionOne",
-    })
-    if err != nil {
-        fmt.Println(err)
-    }
-    return client
+	provider := getProvider()
+	client, err := openstack.NewNetworkV2(provider, gophercloud.EndpointOpts{
+		Name:   "neutron",
+		Region: "RegionOne",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	return client
+}
+
+func GetComputeClient() *gophercloud.ServiceClient {
+	provider := getProvider()
+	client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+		Region: "RegionOne",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	return client
 }
 
 func CreateNetwork(client *gophercloud.ServiceClient, name string) *networks.Network {
-    //client := getNetworkClient()
-    netopts := networks.CreateOpts{Name: name, AdminStateUp: networks.Up}
-    network, err := networks.Create(client, netopts).Extract()
-    if err != nil {
-        fmt.Println(err)
-        return nil
-    }
-    return network
+	//client := getNetworkClient()
+	netopts := networks.CreateOpts{Name: name, AdminStateUp: networks.Up}
+	network, err := networks.Create(client, netopts).Extract()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return network
 }
 
-func FindNetwork(client *gophercloud.ServiceClient, uuid string)(bool, networks.Network) {
-    shared := false
-    listopts := networks.ListOpts{Shared: &shared}
-    pager := networks.List(client, listopts)
-    var net networks.Network 
-    err := pager.EachPage(func(page pagination.Page) (bool, error) {
-        networkList, _ := networks.ExtractNetworks(page)
-        for _, n := range networkList {
-            fmt.Println(n)
-            if n.ID == uuid {
-                fmt.Println("!!! Network verified !!!")
-                //result := networks.Delete(client, n.ID)
-                //fmt.Println(result)
-                net = n
-                return true, nil
-            }
-        }
-        return false, nil
-    })
-    if err != nil {
-        fmt.Println(err)
-        return false, net
-    }
-    return true, net
+func CreateSubnet(client *gophercloud.ServiceClient, name string, netid string, cidr string) *subnets.Subnet {
+	opts := subnets.CreateOpts{
+		NetworkID: netid,
+		CIDR:      cidr,
+		IPVersion: subnets.IPv4,
+		Name:      name,
+	}
+	subnet, err := subnets.Create(client, opts).Extract()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return subnet
+}
+
+func CreateCompute(client *gophercloud.ServiceClient, name string, netid string) *servers.Server {
+	var opts servers.CreateOpts
+	if netid == "" {
+		opts = servers.CreateOpts{
+			Name:      name,
+			ImageRef:  "7bc33401-17c4-4755-80d7-49ce1bf7d53d",
+			FlavorRef: "42",
+		}
+	} else {
+		net := servers.Network{UUID: netid}
+		opts = servers.CreateOpts{
+			Name:      name,
+			ImageRef:  "7bc33401-17c4-4755-80d7-49ce1bf7d53d",
+			FlavorRef: "42",
+			Networks:  []servers.Network{net},
+		}
+	}
+	compute, err := servers.Create(client, opts).Extract()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return compute
+}
+
+func FindNetwork(client *gophercloud.ServiceClient, uuid string) (bool, networks.Network) {
+	shared := false
+	listopts := networks.ListOpts{Shared: &shared}
+	pager := networks.List(client, listopts)
+	var net networks.Network
+	err := pager.EachPage(func(page pagination.Page) (bool, error) {
+		networkList, _ := networks.ExtractNetworks(page)
+		for _, n := range networkList {
+			if n.ID == uuid {
+				net = n
+				return true, nil
+			}
+		}
+		return false, nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return false, net
+	}
+	return true, net
 }
 
 func DeleteNetwork(client *gophercloud.ServiceClient, uuid string) bool {
-    result := networks.Delete(client, uuid)
-    if result.ExtractErr() != nil {
-        fmt.Println(result)
-        return false
-    }
-    return true
-
+	result := networks.Delete(client, uuid)
+	if result.ExtractErr() != nil {
+		fmt.Println(result)
+		return false
+	}
+	return true
 }
 
+func DeleteCompute(client *gophercloud.ServiceClient, uuid string) bool {
+	result := servers.Delete(client, uuid)
+	if result.ExtractErr() != nil {
+		fmt.Println(result)
+		return false
+	}
+	return true
+}
 
-func CheckNeutronEOS(uuids []string) {
+// Checks if EOS is aware of the network
+
+func CheckNeutronEOS(url string, uuid string) bool {
+	// show openstack networks network <string>
+	// if regions is not empty then network entry exists
+	n := "show openstack networks network " + uuid
+	cmds := []string{n}
+	r := eapi.Call(url, cmds, "json")
+	nets := r.Result[0]["regions"].(map[string]interface{})
+	if len(nets) > 0 {
+		return true
+	}
+	return false
 }
-func CheckNovaEOS(uuids []string) {
+
+func CheckNovaEOS(url string, uuid string) bool {
+	// show openstack vms vm <string>
+	// if regions is not empty then network entry exists
+	n := "show openstack vms vm " + uuid
+	cmds := []string{n}
+	r := eapi.Call(url, cmds, "json")
+	vms := r.Result[0]["regions"].(map[string]interface{})
+	if len(vms) > 0 {
+		return true
+	}
+	return false
 }
+
 func CheckRtrEOS() {
 }
-
 
 func CreateRouter() {
 }
 
 func RemoveRouter() {
-}
-
-func CreateNova() {
-}
-
-func RemoveNova() {
 }
--- openstack_test.go
+++ openstack_test.go
@@ -1,48 +1,119 @@
 package aristalabstatus
 
 import (
+	"fmt"
+	"github.com/rackspace/gophercloud/openstack/compute/v2/servers"
+	"github.com/rackspace/gophercloud/openstack/networking/v2/networks"
 	"testing"
+	"time"
 )
 
 func TestCreateNetwork(t *testing.T) {
-    client := GetNetworkClient()
-    name := "test_create"
-    net := CreateNetwork(client, name)
-    if net == nil {
-        t.Errorf("Got nil for netowrk creation")
-    }
-    if net.Name != name {
-        t.Errorf("Network name is {}", net.ID)
-    }
+	client := GetNetworkClient()
+	name := "test_create"
+	net := CreateNetwork(client, name)
+	if net == nil {
+		t.Errorf("Got nil for netowrk creation")
+	}
+	if net.Name != name {
+		t.Errorf("Network name is {}", net.ID)
+	}
+	DeleteNetwork(client, net.ID)
+}
+
+func TestCompute(t *testing.T) {
+	nclient := GetNetworkClient()
+	cclient := GetComputeClient()
+	name := "test_create_vm"
+	net := CreateNetwork(nclient, name)
+	sn := CreateSubnet(nclient, "test_create_sn", net.ID, "192.168.187.0/24")
+	if sn == nil {
+		t.Errorf("Error creating subnet")
+	}
+	compute := CreateCompute(cclient, name, net.ID)
+	if compute == nil {
+		t.Errorf("Got nil for compute creation")
+	}
+	success := DeleteCompute(cclient, compute.ID)
+	if !success {
+		t.Errorf("Error in removing vm")
+	}
+	DeleteNetwork(nclient, net.ID)
 }
 
 func TestFindNetwork(t *testing.T) {
-    client := GetNetworkClient()
-    name := "test_find"
-    net := CreateNetwork(client, name)
-    found, n := FindNetwork(client, net.ID)
-    if !found {
-        t.Errorf("Network not found")
-    }
-    if net.ID != n.ID {
-        t.Errorf("Network name is %s, should be %s", net.ID, n.ID)
-    }
+	client := GetNetworkClient()
+	name := "test_find"
+	net := CreateNetwork(client, name)
+	found, n := FindNetwork(client, net.ID)
+	if !found {
+		t.Errorf("Network not found")
+	}
+	if net.ID != n.ID {
+		t.Errorf("Network name is %s, should be %s", net.ID, n.ID)
+	}
+	DeleteNetwork(client, net.ID)
 }
 
 func TestDeleteNetwork(t *testing.T) {
-    client := GetNetworkClient()
-    name := "test_remove"
-    net := CreateNetwork(client, name)
-    found, n := FindNetwork(client, net.ID)
-    if !found {
-        t.Errorf("Network not found")
-    }
-    if net.ID != n.ID {
-        t.Errorf("Network name is %s, should be %s", net.ID, n.ID)
-    }
-    success := DeleteNetwork(client, net.ID)
-    if !success {
-        t.Errorf("Error in removing")
-    }
+	client := GetNetworkClient()
+	name := "test_remove"
+	net := CreateNetwork(client, name)
+	found, n := FindNetwork(client, net.ID)
+	if !found {
+		t.Errorf("Network not found")
+	}
+	if net.ID != n.ID {
+		t.Errorf("Network name is %s, should be %s", net.ID, n.ID)
+	}
+	success := DeleteNetwork(client, net.ID)
+	if !success {
+		t.Errorf("Error in removing network")
+	}
 }
+
 // Create a cleanup test to remove all test networks
+
+func TestNeutronEOS(t *testing.T) {
+	url := "https://admin:admin@bleaf1/command-api/"
+
+	if CheckNeutronEOS(url, "bogus") {
+		t.Errorf("Fake network created in EOS")
+	}
+	client := GetNetworkClient()
+	name := "test_eos_net"
+	net := CreateNetwork(client, name)
+	if !CheckNeutronEOS(url, net.ID) {
+		t.Errorf("Neutron network " + net.ID + " not created in EOS")
+	}
+	DeleteNetwork(client, net.ID)
+}
+
+func TestNovaEOS(t *testing.T) {
+	// nclient := GetNetworkClient()
+	// cclient := GetComputeClient()
+
+	url := "https://admin:admin@bleaf1/command-api/"
+	if CheckNovaEOS(url, "bogus") {
+		t.Errorf("Fake vm reported in EOS")
+	}
+	net, vm := CreateNetCompute()
+	time.Sleep(5 * time.Second)
+	if !CheckNovaEOS(url, vm.ID) {
+		t.Errorf("VM " + vm.ID + " not created in EOS")
+	}
+	fmt.Println(vm.ID)
+	fmt.Println(net.ID)
+	// DeleteCompute(cclient, vm.ID)
+	// DeleteNetwork(nclient, net.ID)
+}
+
+func CreateNetCompute() (*networks.Network, *servers.Server) {
+	nclient := GetNetworkClient()
+	cclient := GetComputeClient()
+	name := "test_create_vm"
+	net := CreateNetwork(nclient, name)
+	CreateSubnet(nclient, "test_create_sn", net.ID, "192.168.187.0/24")
+	compute := CreateCompute(cclient, name, net.ID)
+	return net, compute
+}
